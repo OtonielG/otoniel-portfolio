4c17ddb7705de560623ae2c6796cbf7d919ee26b
OtonielG <otonielgomez993@gmail.com>
2026-08-25 15:57:05 -0600

    feat: remove About section and associated image for a cleaner layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import About from "@/components/sections/acerca-de";
 import Hero from "@/components/sections/hero";
 import TechCarousel from "@/components/ui/tech-carousel";
 
 export default function Home() {
   return (
-    <div className="">
+    <>
       <Hero />
       <TechCarousel />
-      <About />
-    </div>
+    </>
   );
 }
