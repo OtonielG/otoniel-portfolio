5b0c5fcfaeb92122ac9717fcae062257b70ea977
OtonielG <otonielgomez993@gmail.com>
2026-09-01 17:30:09 -0600

    feat: add animations and structure to Skills component for enhanced visual effects

--- src/components/ui/skills.tsx
+++ src/components/ui/skills.tsx
@@ -1,9 +1,17 @@
 export default function Skills() {
   return (
-    <section className="w-full flex flex-col items-center justify-center gap-5 px-4 py-6 md:py-8 md:scroll-mt-7 lg:scroll-mt-8">
-      <h2 className="font-technor text-display text-3xl sm:text-4xl md:text-5xl xl:text-6xl text-center">
+    <section className="w-full overflow-x-clip flex flex-col items-center justify-center gap-5 py-6 md:py-8 md:scroll-mt-7 lg:scroll-mt-8">
+      <h2 className="font-technor text-display text-3xl sm:text-4xl md:text-5xl xl:text-6xl text-center md:mb-6">
         Habilidades
       </h2>
+
+      <div className="w-[95%] sm:w-[85%] md:w-[80%] lg:w-[70%] xl:w-[65%] 2xl:w-[55%]">
+        <ul className="skill-container w-full flex gap-5 overflow-visible">
+          <li className="skill-card w-1/3 aspect-[3/4] bg-red-100"></li>
+          <li className="skill-card w-1/3 aspect-[3/4] bg-blue-100"></li>
+          <li className="skill-card w-1/3 aspect-[3/4] bg-green-100"></li>
+        </ul>
+      </div>
     </section>
   );
 }
